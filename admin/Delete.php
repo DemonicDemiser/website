0532285d99c115e61d9ffece7aafe14d28ec8c38
DemonicDemiser <waabaki921642@gmail.com>
2021-06-13 13:46:49 +0600

    Complete Optimization of admin page
    ads

--- admin/Delete.php
+++ admin/Delete.php
@@ -7,9 +7,12 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
     <link rel="stylesheet" href="admin.css">
     <title>Admin</title>
+
 </head>
 <body>
 <style>
@@ -112,7 +115,7 @@ session_start();
         <div class="col-12 mb-4">
             <?php require('../navigation.php'); ?>
         </div>
-            <div class="align-self-start col-2 p-2 bg-dark">
+            <div class="align-self-start col-2 px-2 mx-3 bg-dark">
                 <div class="d-flex flex-column">
                     <div class="d-inline text-white flex-column font-weight-bold py-3">
                         <h2 class="hoverr px-5 text-decoration-none ">Category</h2>
@@ -124,35 +127,41 @@ session_start();
                         <a href="Update.php" id="update" class="hoverr px-5 text-decoration-none">Update</a>
                         <a href="Delete.php" id="delete" class="hoverr px-5 text-decoration-none">Delete</a>
                     </div>
+                </div>
             </div>
             <div id="#add" class="d-flex col-8 p-2 d-block">
                 <div class="container w-100  mx-5 h-100">
-                <div class="row">
-                    <div class="col-75">
-                        <div class="container">
-                            <form action="Delete_handler.php" method="post">
-                            <div class="row">
-                                <div class="col-50">
-                                <h3>Delete</h3>
-                                <label for="id"><i class="fa fa-user"></i>ID</label>
-                                <input type="number" id="id" name="id" placeholder="id">
+                    <div class="row">
+                        <div class="col-75">
+                            <div class="container">
+                                <form action="Delete_handler.php" method="post">
+                                <div class="row">
+                                    <div class="col-50">
+                                    <h3>Delete</h3>
+                                    <label for="id"><i class="fa fa-user"></i>ID</label>
+                                    <input type="number" id="id" name="id" placeholder="id">
+                                    </div>
                                 </div>
+                                <input type="submit" value="Add" class="btne border">
+                                </form>
                             </div>
-                            <input type="submit" value="Add" class="btne border">
-                            </form>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
         <div class="col-2 p-2 text-dark">s</div>
         <div class="dot bg-white"></div>
     </div>
 </div>
 <script src="jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
 <?php
